6a14ccaa062b10024f2cae4a089ac6e4d2566dec
ashaqmir <mir.ashaq.ali@gmail.com>
2018-12-05 07:58:47 -0500

    Some minor cleanup

--- mc/CodeAnalysis/SyntexToken.cs
+++ mc/CodeAnalysis/SyntexToken.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace Minsk.CodeAnalysis
 {
-    class SyntaxToken : SyntaxNode
+    public sealed class SyntaxToken : SyntaxNode
     {
         public SyntaxToken(SyntaxKind kind, int _position, string text, object value)
         {
